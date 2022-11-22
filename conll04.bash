9d47886f386db215d4f306dee97880a2c9666691
Italo Silva <italo@maleldil.com>
2022-11-22 08:39:05 +0000

    Create script for CONLL04 so commands aren't bash-specific

---
+++ conll04.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+source config/data_conf/large_conll04_conf.ini 
+export model_name="luyaojie/uie-base-en" 
+export dataset_name=relation/conll04 
+bash scripts_exp/run_exp.bash
