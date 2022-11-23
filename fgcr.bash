73773d0075ef7ebc2910b0c8425ff47d6d225428
Italo Silva <italo@maleldil.com>
2022-11-23 17:24:57 +0000

    Change configuration file on fgcr.bash to the FGCR config

--- fgcr.bash
+++ fgcr.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-source config/data_conf/large_conll04_conf.ini
+source config/data_conf/large_fgcr_conf.ini
 export model_name="luyaojie/uie-base-en"
 export dataset_name=relation/fgcr
 bash scripts_exp/run_exp.bash
