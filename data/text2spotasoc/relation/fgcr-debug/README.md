e94d55e55d975e5bc5d5f00fb6c200dde6da3837
Italo Silva <italo@maleldil.com>
2022-11-24 07:58:28 +0000

    Add full FGCR dataset and add flag to finetune script

---
+++ data/text2spotasoc/relation/fgcr-debug/README.md
@@ -0,0 +1,12 @@
+# Fine-Grained Causality Reasoning (FGCR) dataset
+The files in this folder are derived from the data from the [Towards
+Fine-grained Causal Reasoning and QA](https://arxiv.org/pdf/2204.07408.pdf)
+paper by Linyi Yang, Zhen Wang, Yuxiang Wu, Jie Yang and Yue Zhang, licensed
+under CC-BY-NC-SA.
+
+You can find the original dataset here:
+https://github.com/YangLinyi/Fine-grained-Causal-Reasoning
+
+The scripts used to transform the data are
+`dataset_processing/scripts/fgcr_processing.py` and
+`dataset_processing/run_fgcr_data_generation.bash`.
--- fgcr.bash
+++ fgcr.bash
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
+
+mode='debug'
+
+print_usage() {
+	echo "Usage: fgcr [OPTION]"
+	echo "  -h    Print this message"
+	echo "  -f    Use the full dataset. If absent, uses a debug version with" \
+		 " 10 samples only."
+}
+
+while getopts 'fh' flag; do
+	case "${flag}" in
+		f) mode='full' ;;
+		h) print_usage
+		   exit ;;
+	esac
+done
+
+if [[ "$mode" == 'full' ]]; then
+	dataset_name=relation/fgcr
+else
+	dataset_name=relation/fgcr-debug
+fi
+
+echo "dataset_name=${dataset_name}"
 
 source config/data_conf/large_fgcr_conf.ini
 export model_name="luyaojie/uie-base-en"
-export dataset_name=relation/fgcr
+export dataset_name="${dataset_name}"
 bash scripts_exp/run_exp.bash
